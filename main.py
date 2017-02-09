e05965fe9ca0303b62b9f8536c1e4977895f62e1
Norah Lovelace <call1800norah@gmail.com>
2017-02-08 21:41:07 -0600

    build ViewPostHandler and two html file

--- main.py
+++ main.py
@@ -18,21 +18,68 @@ import webapp2
 import cgi
 import jinja2
 import os
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
-class MainHandler(webapp2.RequestHandler):
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    content = db.TextProperty(required = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+class Handler(webapp2.RequestHandler):
+    def renderError(self, error_code):
+        self.error(error_code)
+        self.response.write("We need both a subject and some contents in blog!")
+
+class MainBlogHandler(handler):
     def get(self):
-        t = jinja_env.get_template("base.html")
-        content = t.render()
-        self.response.write(content)
+       posts = db.GqlQuery("SELECT * FROM Post order bt created desc")
+       t = jinja_env.get_template("base.html")
+       content = t.render(posts=posts,error = self.request.get("error"))
+       self.response.write(content)
 
 class ViewPostHandler(webapp2.RequestHandler):
+    def renderError(self, error_code):
+        self.error(error_code)
+        self.response.write("We need both a subject and some contents in blog!")
+
     def get(self,id):
-        self.response.write()      
+        key = Post.get_by_id(int(id))
+
+        if not key:
+            self.renderError(400)
+            return
+
+        self.response.write("new_blog.html",key = key)
+
+class NewPostHandler(webapp2.RequestHandler):
+    def get(self,subject="",blog="",error=""):
+        t = jinja_env.get_template("new_posts.html")
+        content = t.render(subject=subject,blog=blog,error=error)
+        self.response.write(content)
+
+    def post(self):
+        subject = self.request.get("subject")
+        blog = self.request.get("blog")
+
+        if subject and blog:
+            self.response.write("thanks!")
+        else:
+            error = "We need both a subject and some contents in blog!"
+            self.response.write(subject=subject,blog=blog,error=error)
+
+
+
+
 
 app = webapp2.WSGIApplication([
-      ('/', MainHandler),
-      webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+       webapp2.Route('/', Handler),
+       webapp2.Route('/blog', MainBlogHandler),
+       webapp2.Route('/blog/<id:\d+>', ViewPostHandler),
+       webapp2.Route('/blog/newpost', NewPostHandler),
+
       ], debug=True)
